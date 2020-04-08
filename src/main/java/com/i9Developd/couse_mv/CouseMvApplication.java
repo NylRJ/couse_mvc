518d4ebec2fbfefff50426a399b2a782017b3aa8
Moisés <loveloungeoficial@gmail.com>
2020-04-08 05:55:57 -0300

    Create Project mvc comsumer to API_couse

---
+++ src/main/java/com/i9Developd/couse_mv/CouseMvApplication.java
@@ -0,0 +1,13 @@
+package com.i9Developd.couse_mv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouseMvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouseMvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/i9Developd/couse_mv/CouseMvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i9Developd.couse_mv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouseMvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
